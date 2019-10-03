627f1e1eced22a3956eb69a9d795c1587560fb79
BaronInjusticia <antonio.quetzatlcoatl@gmail.com>
2019-10-03 13:54:04 -0500

    Sedes listo

--- src/views/Sedes.vue
+++ src/views/Sedes.vue
@@ -1,5 +1,28 @@
 <template>
-    <div>
-        <h1>Sedes</h1>  
-    </div>
+
+<div>
+  <b-card-group>
+    <b-card title="Castillo de Chapultepec" img-src="https://cdn3.m.admexico.mx/uploads/images/thumbs/mx/ad/1/s/2018/39/castillo_chapultepec_casa_descanso_museo_historia_2612_600x.jpg" img-alt="Image" img-top>
+      <template v-slot:footer>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.814027438535!2d-99.18412368459504!3d19.420439686891203!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1fecd47ed8f23%3A0xa6e0008524818b32!2sCastillo%20de%20Chapultepec!5e0!3m2!1ses-419!2smx!4v1570128488851!5m2!1ses-419!2smx" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+    </template>
+    </b-card>
+
+    <b-card title="Templo Mayor" img-src="https://d36tnp772eyphs.cloudfront.net/blogs/2/2019/01/Templo-mayor-ciudad-de-mexico-940x626.jpg" img-alt="Image" img-top>
+
+      <template v-slot:footer>      
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3762.4846113976687!2d-99.1340513845949!3d19.43466208688311!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f8dbfaacaa91%3A0xe30b2d354298d3f6!2sTemplo%20Mayor!5e0!3m2!1ses-419!2smx!4v1570128563711!5m2!1ses-419!2smx" width="300" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+
+      </template>
+    </b-card>
+
+    <b-card title="Auditorio Piña Chan" img-src="https://i1.wp.com/noticieros.televisa.com/wp-content/uploads/2018/09/asamblea-interuniversitaria-2.jpg?resize=995%2C602&quality=95&ssl=1" img-alt="Image" img-top>
+
+      <template v-slot:footer>        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3765.535572219284!2d-99.18168378459649!3d19.30255268695837!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85ce0101f94e8033%3A0x3784f63ac347fe3c!2sAuditorio%20Rom%C3%A1n%20Pi%C3%B1a%20Chan!5e0!3m2!1ses-419!2smx!4v1570128582883!5m2!1ses-419!2smx" width="290" height="300" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      </template>
+    </b-card>
+  </b-card-group>
+</div>
+
 </template>
